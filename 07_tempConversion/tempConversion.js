837d8d0319a3b76c1090921886df73c534660999
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-08-30 09:00:34 +0800

    Finish 10 javascript exercises of The Odin Project

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  const celsius = (number - 32) * 5 / 9;
+  let output = Math.round(celsius * 10) / 10;
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  const fahrenheit = (number * ( 9 / 5 ) ) + 32;
+  let output = Math.round(fahrenheit * 10) / 10;
+  return output;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,17 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	let sum = 0;
+  myArray.forEach(element => {
+    sum += element;
+  })
+  return sum;
 };
 
 const multiply = function() {
